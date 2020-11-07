56c7a3eac42f9f11fb69011e7b3c2e91c2f1ee05
Khoa <henry.tonthat.2014@gmail.com>
2020-11-07 17:10:39 -0600

    Had the files in the wrong folder! LOL

---
+++ src/Avo.java
@@ -0,0 +1,21 @@
+public class Avo extends Tavunu {
+
+    public Avo(String name, int birthYear, int pava)
+    {
+        super(name, birthYear, pava);
+        if(pava > 20)
+        {
+            throw new IllegalArgumentException("invalid pava amount");
+        }
+
+        if(pava < 10)
+        {
+            this.pava = 10;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " born in " + birthYear + " is an Avo with " + pava + " pava.";
+    }
+}
---
+++ src/CrodeExalted.java
@@ -0,0 +1,37 @@
+public class CrodeExalted extends Crode{
+    public CrodeExalted(String name, int birthYear, int pava) {
+        // pass 1 as birthYear to Crode ctor so name and pava are configured
+        super(name, 1, pava);
+
+        // then check birthYear parameter to see if we have a valid CrodeExalted
+        if(birthYear%2 == 1)
+        {
+            throw new IllegalArgumentException("invalid year");
+        }
+
+        else
+        {
+            this.birthYear = birthYear;
+        }
+
+        if(pava <= 80)
+        {
+            throw new IllegalArgumentException("invalid pava amount");
+        }
+
+
+    }
+    @Override
+    public void setBirthYear(int birthYear) {
+        if(birthYear%2 == 1)
+        {
+            throw new IllegalArgumentException("invalid year");
+        }
+        else
+            this.birthYear=birthYear;
+    }
+    @Override
+    public String toString() {
+        return name + " born in " + birthYear + " is an Exalted Crode with " + pava + " pava.";
+    }
+}
