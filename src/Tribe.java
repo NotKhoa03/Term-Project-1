1b534c21b7634b81a3c2e3f8491bd4de9d3e0958
Khoa <henry.tonthat.2014@gmail.com>
2020-11-30 19:01:59 -0600

    System inputs and outputs

--- src/Tribe.java
+++ src/Tribe.java
@@ -4,26 +4,51 @@ import java.lang.*;
 public class Tribe {
     public static void main(String[] args){
 
-        int choice;
+        int choice = 0;
+        String name = "ddd";
+        int birthyear = 0;
+        int pava = 0;
 
-        System.out.println("Choose from the following:");
+        while(choice != 6) {
+            System.out.println("Choose from the following:");
 
-        System.out.println("1. Add a member \n2. Remove a member \n3. Display the tribe \n4. Save the tribe \n5. Open the tribe \n6. Quit");
+            System.out.println("1. Add a member \n2. Remove a member \n3. Display the tribe \n4. Save the tribe \n5. Open the tribe \n6. Quit");
 
-        Scanner sc = new Scanner(System.in);
-        choice = sc.nextInt();
+            Scanner sc = new Scanner(System.in);
+            choice = sc.nextInt();
 
-        switch(choice){
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
+            switch (choice) {
+                case 1: {
+                    System.out.println("Enter name of tavuna:");
+                    name = sc.next();
+                    sc.nextLine();
 
+                    System.out.println("Enter birthyear:");
+                    birthyear = sc.nextInt();
+
+                    System.out.println("Enter amount of pava:");
+                    pava = sc.nextInt();
+
+                    System.out.println(name + " " + birthyear + " " + pava);
+                    break;
+                }
+
+                case 2: {
+                    System.out.println("Enter which member you want to remove:");
+                    break;
+                }
+                case 3:
+
+                case 4: {
+                    System.out.println("Saved to file.");
+                    break;
+                }
+                case 5: {
+                    System.out.println("There are some tribe members in memory, do you want to continue and lose the data?");
+                    break;
+                }
+                case 6:
+            }
         }
-
-
-
     }
 }
